14bfbf574f2c8f9a746d72255ae02fb9bfad7e49
Andy Xiang <andy@iden2.com>
2025-01-28 08:31:24 -0800

    adding header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 
 // pages/_app.tsx
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes'
+import Layout from '../components/Layout'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <ThemeProvider attribute="class">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      </ThemeProvider>
+  )
 }
